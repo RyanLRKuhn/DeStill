ce6be246137099191a8c522671f1e0e73d510719
Ryan Kuhn <ryan.lr.kuhn@gmail.com>
2026-03-15 00:05:12 -0400

    clean repo

--- test_main.js
+++
@@ -1,4 +0,0 @@
-const e = require('electron');
-console.log('typeof electron:', typeof e);
-console.log('electron keys:', typeof e === 'object' ? Object.keys(e).slice(0, 5) : 'not object');
-console.log('app:', typeof e.app);
